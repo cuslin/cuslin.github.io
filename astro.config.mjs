b6f936174888979d405a62b17e839e64062afeb2
flurie <alex@flurie.net>
2025-04-25 16:29:52 -0400

    add domain to site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cuslin.github.io",
+  site: "https://www.cumslutincest.com",
   integrations: [mdx(), sitemap()],
 });
